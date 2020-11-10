ee552222408a4ee12cf2240be37c612aa351286e
Lin Hao Lee <lhlee888@hotmail.com>
2020-11-10 20:51:10 +0000

    webpack serve issue

--- src/containers/AppLayout/AppLayout.tsx
+++ src/containers/AppLayout/AppLayout.tsx
@@ -1,18 +1,16 @@
 import React, { useEffect } from 'react'
 import Sheet from '../../components/Sheet/Sheet'
 import { Button } from 'antd'
-import Loading from '../../components/Loading/Loading'
 import { useLoadingState } from '../../contexts/loadingState'
+import Loading from '../../components/Loading/Loading'
 
 const AppLayout = () => {
-  const [loadingState] = useLoadingState()
-  const { loading } = loadingState
 
   return (
     <div>
       <Button type="ghost">Test</Button>
       <Sheet file="C:\Users\lhlee\Documents\flippy-fyp\flippy-electron\tmp\test.xml" />
-      {/* {loading && <Loading/>} */}
+      <Loading />
     </div>
   )
 }
