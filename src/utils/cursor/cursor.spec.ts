43fb148a4018625650f8687e1e5ebcd303ee86a7
lhl2617 <lhlee888@hotmail.com>
2020-12-28 02:12:05 +0000

    moveCursor implemented

--- src/utils/cursor/cursor.spec.ts
+++ src/utils/cursor/cursor.spec.ts
@@ -2,9 +2,9 @@ import * as assert from 'assert';
 import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay'
 import { getDivElement, preludeTestFile, getScore } from '../testutils/testutils';
 import preludeCursorTimingsJson from '../../testxml/prelude-cursor-timings.test.json'
-import { CursorProcessor } from './cursor';
+import { CursorProcessor, CursorTiming, getRequiredStep } from './cursor';
 
-suite('Test initCursorTimings', () => {
+suite(`Test CursorProcessor`, () => {
     test(`Test with BWV846 Prelude`, async () => {
         try {
             const osmd = new OpenSheetMusicDisplay(getDivElement(document), {autoResize: false})
@@ -16,7 +16,7 @@ suite('Test initCursorTimings', () => {
             await osmd.load(scorePath)
             osmd.render()
 
-            const cProc = new CursorProcessor(osmd)
+            const cProc = new CursorProcessor(osmd.cursor)
             
             const got = cProc.getCursorTimings()
             const want = preludeCursorTimingsJson
@@ -27,4 +27,69 @@ suite('Test initCursorTimings', () => {
             assert.fail(err)
         }
     }).timeout(10000)
+})
+
+suite(`Test getRequiredStep`, () => {
+    const cursorTimings: CursorTiming[] = [
+        {
+            stepNumber: 0,
+            timestamp: 0,
+        },
+        {
+            stepNumber: 1,
+            timestamp: 1,
+        },
+        {
+            stepNumber: 2,
+            timestamp: 2,
+        },
+        {
+            stepNumber: 3,
+            timestamp: 3,
+        },
+    ]
+    const cases = [
+        {
+            name: "Zero",
+            timestamp: 0,
+            want: 0,
+        },
+        {
+            name: "Two",
+            timestamp: 2,
+            want: 2,
+        },
+        {
+            name: "Tie and prefer front",
+            timestamp: 0.5,
+            want: 0,
+        },
+        {
+            name: "Closer to next",
+            timestamp: 0.51,
+            want: 1,
+        },
+        {
+            name: "Closer to prev",
+            timestamp: 0.49,
+            want: 0,
+        },
+        {
+            name: "Too large",
+            timestamp: 4.1,
+            want: 3,
+        },
+        {
+            name: "Too small",
+            timestamp: -1,
+            want: 0,
+        },
+    ]  
+
+    for (const tc of cases) {
+        test(tc.name, () => {
+            const got = getRequiredStep(cursorTimings, tc.timestamp)
+            assert.equal(got, tc.want)
+        })
+    }
 })
