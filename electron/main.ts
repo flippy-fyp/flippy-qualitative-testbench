d52453d77aa9da3453d221900b14458de4043e7f
lhl2617 <lhlee888@hotmail.com>
2021-01-18 23:20:47 +0000

    change title and lint electron file

--- electron/main.ts
+++ electron/main.ts
@@ -1,46 +1,47 @@
-import { app, BrowserWindow, protocol } from "electron";
-import * as path from "path";
-import * as url from "url";
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import { app, BrowserWindow, protocol } from 'electron'
+import * as path from 'path'
+import * as url from 'url'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
-let mainWindow: Electron.BrowserWindow | null;
+let mainWindow: Electron.BrowserWindow | null
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 720,
-    webPreferences: {
-      enableRemoteModule: true,
-      nodeIntegration: true,
-      webSecurity: false,
-    },
-  });
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            enableRemoteModule: true,
+            nodeIntegration: true,
+            webSecurity: false,
+        },
+        title: 'Flippy Qualitative Testbench',
+    })
 
-  if (process.env.NODE_ENV === "development") {
-    mainWindow.loadURL(`http://localhost:4000`);
-    mainWindow.webContents.openDevTools();
-  } else {
-    mainWindow.loadURL(
-      url.format({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file:",
-        slashes: true,
-      })
-    );
-  }
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.loadURL(`http://localhost:4000`)
+        mainWindow.webContents.openDevTools()
+    } else {
+        mainWindow.loadURL(
+            url.format({
+                pathname: path.join(__dirname, 'index.html'),
+                protocol: 'file:',
+                slashes: true,
+            })
+        )
+    }
 
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
 }
 
-app.on("ready", createWindow);
-app.allowRendererProcessReuse = true;
+app.on('ready', createWindow)
+app.allowRendererProcessReuse = true
 
 app.whenReady().then(() => {
-  protocol.registerFileProtocol('file', (request, callback) => {
-    const pathname = request.url.replace('file:///', '');
-    callback(pathname);
-  });
-});
+    protocol.registerFileProtocol('file', (request, callback) => {
+        const pathname = request.url.replace('file:///', '')
+        callback(pathname)
+    })
+})
