de8a63bcb94148583977eb2d08b92b438c0ed2fe
lhl2617 <lhlee888@hotmail.com>
2021-05-11 00:12:47 +0100

    Link App title to Repo

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,8 @@ import AppLayout from "./containers/AppLayout/AppLayout"
 import { LoadingStateProvider } from "./contexts/loadingState"
 import Loading from "./components/Loading/Loading"
 import "antd/dist/antd.css"
+import "./app.css"
+
 const mainElement = document.createElement("div")
 document.body.appendChild(mainElement)
 
